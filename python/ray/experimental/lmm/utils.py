a598d48eca338689b82965a19dd42e6107efe4d8
scv119 <scv119@gmail.com>
2023-02-24 22:12:10 -0800

    funfunfun

---
+++ python/ray/experimental/lmm/utils.py
@@ -0,0 +1,49 @@
+import ray
+import time
+
+@ray.remote(num_gpu=1)
+class GpuActor:
+    def __init__(self, id, model):
+        self.id = id
+        self.model = model
+        pass
+
+    def forward(self, input):
+        return 
+
+    def backward(self, grad):
+        pass
+
+
+def _forward(layer_id, input):
+    return ray.get_actor(layer_id).forward(input)
+
+def _backward(layer_id, grad):
+    return ray.get_actor(layer_id).forward(input)
+
+
+class RayDataMode:
+    def __init__(self, layers):
+        for layer_id, layer in enumerate(layers):
+            self.layer_actors = [GpuActor.options(name=str(layer_id)).remote(layer_id, layer)]
+
+    def train(self, ds):
+        # forward pass
+        for _ in range(len(self.layer_actors)):
+            ds = ds.map(_forward)
+
+        # loss functions?
+
+        # backward pass
+        for _ in reversed(range(len(self.layer_actors))):
+            ds = ds.map(_backward)
+
+        # ds = ds.map(...)
+
+        # actual training
+        for _ in ds.iter_batches():
+            pass
+
+
+    def inference(self):
+        pass
