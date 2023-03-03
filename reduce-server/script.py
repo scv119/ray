8cfa375bb561c7e78867a7639e9e731d71d630dc
Cade Daniel <edacih@gmail.com>
2023-03-02 20:43:42 -0800

    work on delegating allreduce to cpu reducers

--- reduce-server/script.py
+++ reduce-server/script.py
@@ -2,35 +2,58 @@
 
 import ray
 from collections import defaultdict
+import asyncio
 
 num_gpus = 4
 
 @ray.remote
 class Reducer:
     def __init__(self):
-        self.callbacks = defaultdict(list)
         self.results = {}
         self.inputs = defaultdict(list)
+        self.consumed_count = defaultdict(int)
 
-    def reduce(self, tensor, global_order, num_expected, client_name):
-        #print(f'reduce call from {client_name} {global_order}')
-
+    async def reduce(self, tensor, global_order, num_expected, client_name, client_rank):
+        import torch
         self.inputs[global_order].append(tensor)
-        self.callbacks[client_name].append(global_order)
-        # hand off this to a thread for reduction
-        # once the thread has finished for a particular bucket, then we send it back
-        # to the reducer client.
 
-    def temporary_flush(self):
-        for client_name, invocations in self.callbacks.items():
-            print(client_name, invocations)
-            # TODO file bug for when actor is not stored
-            actor = ray.get_actor(client_name)
-            ray.get(actor.callback.remote(invocations))
+        # The zeroth rank will wait for all inputs, then sum, then store the result.
+        # The non-zero ranks will wait for the zeroth rank to store the result.
+        # The last rank to access the result will delete the results and inputs.
+        if client_rank == 0:
+            while len(self.inputs[global_order]) < num_expected:
+                await asyncio.sleep(0.1)
+        
+            tensors = self.inputs[global_order]
+            
+            sum_tensors = tensors[0]
+            print('summing tensors: ', tensors)
+            for t in tensors[1:]:
+                sum_tensors = torch.add(sum_tensors, t)
 
-        self.callbacks.clear()
-        self.inputs.clear()
-        self.results.clear()
+            #sum_tensors = f'sum for global order {global_order}'
+            self.results[global_order] = sum_tensors
+            self.consumed_count[global_order] += 1
+            result = sum_tensors
+        else:
+            while self.consumed_count[global_order] == 0:
+                await asyncio.sleep(0.1)
+            result = self.results[global_order]
+            self.consumed_count[global_order] += 1
+
+            if self.consumed_count[global_order] == num_expected:
+                del self.inputs[global_order]
+                del self.consumed_count[global_order]
+                del self.results[global_order]
+                print(f'released global_order {global_order}')
+
+        # we have all of them.
+        return result
+
+    def assert_no_leaks(self):
+        assert not self.inputs, f"inputs has elements when it shouldn't {self.inputs}"
+        assert not self.consumed_count, f"consumed_count has elements when it shouldn't {self.consumed_count}"
+        assert not self.results, f"results has elements when it shouldn't {self.results}"
 
 class MonotonicCounter:
     def __init__(self):
@@ -42,25 +65,33 @@ class MonotonicCounter:
 
 @ray.remote
 class ReducerClient:
-    def __init__(self, name, reducer):
+    def __init__(self, name, reducer, rank):
+        import os
+        del os.environ['CUDA_VISIBLE_DEVICES']
+
         self.reducer = reducer
         self.name = name
         self.counter = MonotonicCounter()
+        self.rank = rank
 
     # make into async actor.
-    def allreduce(self, gpu_buffer):
+    async def allreduce(self, gpu_buffer):
         # assign some global order id
         # copy to CPU
         # copy to reducer
         # get result from reducer
         # copy to GPU (gpu_buffer)
-        tensor = None
-        ray.get(self.reducer.reduce.remote(
-            tensor,
+
+        cpu_tensor = gpu_buffer.to('cpu')
+        reduced = await self.reducer.reduce.remote(
+            cpu_tensor,
             self.counter.get_and_increment(),
             num_gpus,
-            self.name
-        ))
+            self.name,
+            self.rank,
+        )
+
+        print(f'reduced:"{reduced}"')
 
     def callback(self, invocations):
         print('callback', invocations)
@@ -70,19 +101,24 @@ class ReducerClient:
 class Trainer:
     def __init__(self, name, rank, reducer):
         self.reducer = reducer
-        self.reducer_client = ReducerClient.options(name=f"reducer_{name}").remote(f"reducer_{name}", reducer)
+        self.reducer_client = ReducerClient.options(name=f"reducer_{name}").remote(f"reducer_{name}", reducer, rank)
         self.rank = rank
 
     def do_training(self):
+        import torch
+        input_tensors = [torch.ones(i+1).cuda() for i in range(2)]
         for epoch in range(10):
-            # create tensor on GPU (assume we're running in SPMD)
-            # call allreduce on tensor
-            ray.get(self.reducer_client.allreduce.remote(None))
+            futures = []
+            for t in input_tensors:
+                futures.append(self.reducer_client.allreduce.remote(t))
+            ray.get(futures)
 
 
 if __name__ == '__main__':
+    print('creating actors')
     reducer = Reducer.remote()
     namegen = lambda i: f"trainer_{i}"
     trainers = [Trainer.options(name=namegen(i)).remote(namegen(i), i, reducer) for i in range(num_gpus)]
+    print('starting training')
     ray.get([t.do_training.remote() for t in trainers])
-    ray.get(reducer.temporary_flush.remote())
+    ray.get(reducer.assert_no_leaks.remote())
